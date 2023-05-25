64cfa55a33c20830532307111df9a74d92ac1144
P van Mill <p.vanmill@student.maastrichtuniversity.nl>
2023-05-25 13:07:56 +0000

    basic outline of problem representation

---
+++ code/Problem.java
@@ -0,0 +1,55 @@
+public class Problem {
+	
+	abstract class ConstraintEvaluator {
+		public int Evaluate(int[] roster); // determines the number of violations of this constraint in the roster
+		public int Contribution(int[] roster, int position); // determines the number of violations of this constraint that one day is involved in
+		public int[] enforce(int[] roster); // removes all violations of this constraint
+	}
+
+	class Contract { // super-evaluator for the constraints of one nurse
+		ConstraintEvaluator[] constraints;
+		double[] ConstraintWeights;
+
+		public int[] enforce(int[] roster, int constraint) {
+
+		}
+		
+		public double Contribution(int[] roster, int position) {
+
+		}
+
+		public double Evaluate(int[] roster) {
+
+		}
+
+	}
+
+	class ProblemInstance {
+		int dayoffset; // 0: Monday, ..., 6: Sunday; weekday of day i is: (i + offset) % 7
+		int N; // number of nurses
+		int D; // number of days
+		int S; // number of shift types
+		int nskills;
+		int[] skillNeeded = new int[S];
+		boolean[] nightShift = new boolean[S];
+
+		Contract[] nurseTypes;
+
+		public int nConstraints;
+
+		double Evaluate(int[] roster, int nurse) {
+
+		}
+
+		public int[] enforce(int constraint, int nurse) {
+
+		}
+		
+		public double Contribution(int[] roster, int position, int nurse) {
+
+		}
+
+		int[][] demands = new int[D][S];
+	}
+
+}
